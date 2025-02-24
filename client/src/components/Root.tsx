7f6af900621d87755fc9ba0394cf99e529c19713
David Fiddes <66165184+UCDFiddes@users.noreply.github.com>
2025-02-24 09:31:11 +0000

    Fix some issues.

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -8,7 +8,7 @@ export default function Root() {
 
   if (!game)
     return (
-      <div className="flex items-center justify-center flex-col min-h-dvh text-center text-zinc-300">
+      <div className="flex items-center justify-center flex-col min-h-dvh text-center text-zinc-300 bg-zinc-800">
         <div className="size-10 mb-3 rounded-full border-6 border-t-transparent border-l-transparent border-white animate-spin" />
         <h2 className="text-xl font-bold">Loading...</h2>
         <span className="text-sm">This shouldn't take long.</span>
